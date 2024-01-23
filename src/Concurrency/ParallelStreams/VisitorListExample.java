d071a080904595d7a55233367b53a9ca78b33258
marwhals <marjan.mubarok2012@gmail.com>
2024-01-23 18:58:24 +0000

    CopyOnWriteArrayList

--- src/Concurrency/ParallelStreams/VisitorListExample.java
+++ src/Concurrency/ParallelStreams/VisitorListExample.java
@@ -6,19 +6,36 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 /*
 Concurrent collections
 - LinkedList, ArrayList, TreeSet and HashSet are not thread-safe
 - They can be made thread safe with the synchronised wrapper which is useful for legacy code
 - NOTE: It is recommended to use concurrent collections for new code
+
+CopyOnWriteArrayList
+- Whenever this list is modified by adding, updating or removing relements an new copy of the underlying array is created
+- The modifications is performed on the new copy, allowing read operations to use the original unmodified array
+- This ensures that reader threads aren't blocked by writers
+- Since changes are made to a separate copy of an array there are not any synchronisation issues between the reading and writing threads
+- This can be costly in some use casses but can be more efficient if the number of read/traversal operations vastly outnumber mutations
+
  */
 
 public class VisitorListExample {
+
+    private static final CopyOnWriteArrayList<Person> masterList;
+
+    static {
+        masterList = Stream.generate(Person::new)
+                .distinct()
+                .limit(2500)
+                .collect(CopyOnWriteArrayList::new,
+                        CopyOnWriteArrayList::add,
+                        CopyOnWriteArrayList::addAll);
+    }
 
     private static final ArrayBlockingQueue<Person> newVisitors =
             new ArrayBlockingQueue<>(5);
@@ -26,9 +43,9 @@ public class VisitorListExample {
     public static void main(String[] args) {
 
         Runnable producer = () -> {
-
+            String threadName = Thread.currentThread().getName();
             Person visitor = new Person();
-            System.out.println("Adding " + visitor);
+            System.out.println(threadName + "  Queueing  " + visitor);
             boolean queued = false;
 
             try {
@@ -37,7 +54,7 @@ public class VisitorListExample {
                 System.out.println("Interrupted Exception!");
             }
             if (queued) {
-                System.out.println(newVisitors);
+//                System.out.println(newVisitors);
             } else {
                 System.out.println("Queue is Full, cannot add " + visitor);
                 System.out.println("Draining Queue and writing data to file");
@@ -56,10 +73,36 @@ public class VisitorListExample {
             }
         };
 
+        Runnable consumer = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " Polling queue " + newVisitors.size());
+            Person visitor = null;
+            try {
+                visitor = newVisitors.take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (visitor != null) {
+                System.out.println(threadName + " " + visitor);
+                if (!masterList.contains(visitor)) {
+                    masterList.add(visitor);
+                    System.out.println("--> New Visitor gets Coupon!: " + visitor);
+                }
+            }
+            System.out.println(threadName + " done " + newVisitors.size());
+        };
+
         ScheduledExecutorService producerExecutor =
                 Executors.newSingleThreadScheduledExecutor();
-        producerExecutor.scheduleWithFixedDelay(producer, 0, 1,
+        producerExecutor.scheduleWithFixedDelay(producer, 0, 3,
                 TimeUnit.SECONDS);
+
+        ScheduledExecutorService consumerPool =
+                Executors.newScheduledThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            consumerPool.scheduleAtFixedRate(consumer, 6, 1,
+                    TimeUnit.SECONDS);
+        }
 
         while (true) {
             try {
@@ -71,5 +114,17 @@ public class VisitorListExample {
         }
 
         producerExecutor.shutdown();
+
+        while (true) {
+            try {
+                if (!consumerPool.awaitTermination(3, TimeUnit.SECONDS))
+                    break;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        consumerPool.shutdown();
+
     }
 }
