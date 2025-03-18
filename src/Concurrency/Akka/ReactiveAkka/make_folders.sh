3a86ae01aa1526471b3ff61e06543aafe5ffb227
marwhals <marjan.mubarok2012@gmail.com>
2025-03-18 22:08:51 +0000

    Set up folder structure for Java based Akka

---
+++ src/Concurrency/Akka/ReactiveAkka/make_folders.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create a directory structure for each chapter
+mkdir "Chapter1_Introduction"
+mkdir "Chapter2_Creating_our_first_stream"
+mkdir "Chapter3_Simple_sources_and_sinks"
+mkdir "Chapter4_simple_flows"
+mkdir "Chapter5_big_primes"
+mkdir "Chapter6_materialised_values"
+mkdir "Chapter7_stream_lifecycles"
+mkdir "Chapter8_logging"
+mkdir "Chapter9_back_pressure_and_graph_performance"
+mkdir "Chapter10_monitoring_vehicle_speed"
+mkdir "Chapter11_graphDSL"
+mkdir "Chapter12_complex_flow_types"
+mkdir "Chapter13_graphs_with_multiple_sources_and_sinks"
+mkdir "Chapter14_non_runnable_or_partial_graphs"
+mkdir "Chapter15_using_actors_in_graphs"
+mkdir "Chapter16_advanced_backpressure"
+mkdir "Chapter17_the_java_flow_package"
+mkdir "Chapter18_blockchain_mining"
