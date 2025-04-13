4d118b065d37b89bc695495c0458e666dd58edeb
marwhals <marjan.mubarok2012@gmail.com>
2025-04-13 15:38:10 +0100

    Implement unit tests

--- src/DebuggingandUnitTesting/BankAccountTest.java
+++ src/DebuggingandUnitTesting/BankAccountTest.java
@@ -1,12 +1,16 @@
 package DebuggingandUnitTesting;
 
 import org.junit.Test;
-import static org.junit.Assert.fail;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
 
 public class BankAccountTest {
     @Test
     public void deposit() throws Exception {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @Test
@@ -15,8 +19,24 @@ public class BankAccountTest {
     }
 
     @Test
-    public void getBalance() throws Exception {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @Test
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertTrue("The account is NOT a checking account", account.isChecking());
+
     }
 
 }
